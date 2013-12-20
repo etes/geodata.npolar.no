4fb2b84d483c60a798d8e1e8658b1447d860ebc6
etes <ermias@npolar.no>
2013-12-20 10:53:44 +0100

    iframe designer 1.1

---
+++ documentation/samples/extras/customoperation.js
@@ -0,0 +1,30 @@
+dojo.provide("myModules.customoperation");
+
+dojo.require("esri.undoManager");
+dojo.require("esri.toolbars.navigation");
+
+dojo.declare("myModules.customoperation.Add", esri.OperationBase, {
+  label: "Add Graphic",
+  constructor: function ( /*graphicsLayer, addedGraphic*/ params) {
+    params = params || {};
+    if (!params.graphicsLayer) {
+      console.error("graphicsLayer is not provided");
+      return;
+    }
+    this.graphicsLayer = params.graphicsLayer;
+
+    if (!params.addedGraphic) {
+      console.error("no graphics provided");
+      return;
+    }
+    this._addedGraphic = params.addedGraphic;
+  },
+
+  performUndo: function () {
+    this.graphicsLayer.remove(this._addedGraphic);
+  },
+
+  performRedo: function () {
+    this.graphicsLayer.add(this._addedGraphic);
+  }
+});
