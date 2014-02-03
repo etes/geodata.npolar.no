44199b52508c190fdb20e124b7483365fe8e9203
etes <ermias@npolar.no>
2014-02-03 17:25:00 +0100

    placenames search using jquery autocomplete

--- js/main.js
+++ js/main.js
@@ -678,6 +678,43 @@ function showhide() {
 
 dojo.ready(init);
 
+//placenames search using jquery autocomplete
+   $(function() {
+    $( "#search-input" ).autocomplete({
+      source: function( request, response ) {
+        $.ajax({
+          url: "http://placenames.npolar.no/stadnamn/edge/",
+          dataType: "jsonp",
+          data: {
+            q: request.term,
+            approved: true,
+            rows: 5,
+            location: "Svalbard",
+          },
+          success: function( data ) {
+            response( $.map( data, function( item ) {
+              return {
+                label: item.title + " (" + item.terrain + ")",
+                value: item.title,
+                object: item
+              };
+            }));
+          }
+        });
+      },
+      minLength: 2,
+      select: function( event, ui ) {
+           WGS84ToUTM33(ui.item.object);
+            addMarker(position);
+      },
+      open: function() {
+        $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+      },
+      close: function() {
+        $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+      }
+    });
+  });
 
 //COLOR PICKER: uses specturm.js
 $("#colorpicker").spectrum({
