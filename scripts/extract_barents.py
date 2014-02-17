3e8b14711befa8aa6c105e64006d44f7ea9a09ae
etes <ermias@npolar.no>
2014-02-17 14:40:04 +0100

    remove old

--- scripts/extract_barents.py
+++ scripts/extract_barents.py
@@ -13,11 +13,6 @@ os.chdir('/home/ermias/protectedplanet')
 
 inShp = "WDPApoly_August2013.shp"
 outShp = "protected_barents.shp"
-query = """
-COUNTRY in ('RUS','NOR', 'SJM')
-"""
-# @TODO find elegant alternative
-os.system('ogr2ogr -f "ESRI Shapefile" -where "' + query + '" -overwrite ' + outShp + ' ' + outShp)
 
 # get the shapefile driver
 driver = ogr.GetDriverByName('ESRI Shapefile')
