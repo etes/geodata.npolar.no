3c053e478ac1c070b5e877af3653d37765e505ca
etes <ermias@npolar.no>
2014-03-18 11:06:36 +0100

    batch project shapefiles to etrs89

--- bin/w84_to_etrs89.py
+++ bin/w84_to_etrs89.py
@@ -4,6 +4,7 @@ Created on 10. feb. 2014
 
 @author: ermias
 '''
+import os
 import arcpy
 from arcpy import env
 connDir = "C:/Users/ermias/AppData/Roaming/ESRI/Desktop10.2/ArcCatalog/"
@@ -32,3 +33,15 @@ if not arcpy.Exists(out_workspace):
     arcpy.CreateFileGDB_management(os.path.dirname(out_workspace), os.path.basename(out_workspace))
 
 arcpy.BatchProject_management(input_features, out_workspace, out_cs, "#","#")
+
+def w84_to_etrs89 (indir, outdir):
+    out_cs = "PROJCS['ETRS_1989_UTM_Zone_33N',GEOGCS['GCS_ETRS_1989',DATUM['D_ETRS_1989',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Transverse_Mercator'],PARAMETER['False_Easting',500000.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',15.0],PARAMETER['Scale_Factor',0.9996],PARAMETER['Latitude_Of_Origin',0.0],UNIT['Meter',1.0]]"
+    env.workspace = indir
+    input_features =  arcpy.ListFeatureClasses()
+    out_workspace = outdir + "/" + os.path.basename(indir).lower() + ".gdb"
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+    if not arcpy.Exists(out_workspace):
+        arcpy.CreateFileGDB_management(os.path.dirname(out_workspace), os.path.basename(out_workspace))
+    arcpy.BatchProject_management(input_features, out_workspace, out_cs, "#","#")
+    print "projecting: ", input_features
