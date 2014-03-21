9e4c3b2568146d5ad4ca7d22955f5a227e71be38
etes <ermias@npolar.no>
2014-03-21 15:00:04 +0100

    update workspace directory of map documents

---
+++ bin/mxd_update_workspace.py
@@ -0,0 +1,17 @@
+'''
+Created on 21. mars 2014
+
+@author: ermias
+'''
+
+import arcpy, os
+folderPath = r"W:\NP\SvalbardTema"
+for filename in os.listdir(folderPath):
+    fullpath = os.path.join(folderPath, filename)
+    if os.path.isfile(fullpath):
+        basename, extension = os.path.splitext(fullpath)
+        if extension.lower() == ".mxd":
+            mxd = arcpy.mapping.MapDocument(fullpath)
+            mxd.findAndReplaceWorkspacePaths(r"E:\Data", r"\\willem2\Data")
+            mxd.save()
+del mxd
