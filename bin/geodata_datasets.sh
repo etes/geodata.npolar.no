38e74c6503f6674b84465a239e55576174e1663a
etes <ermias@npolar.no>
2014-03-21 11:49:06 +0100

    archive datasets

--- bin/geodata_datasets.sh
+++ bin/geodata_datasets.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+DIR=~/backup/psql
+[ ! $DIR ] && mkdir -p $DIR || :
+LIST=$(psql -l | awk '{ print $1}' | grep -vE '^-|^List|^Name|template[0|1]')
+for d in $LIST
+do
+  pg_dump $d | gzip -c >  $DIR/$d.out.gz
+done
+
 pg_dump -C -h test_database -U username -t table dbname | psql -h prod_database -U username dbname
