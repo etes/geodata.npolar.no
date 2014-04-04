05abfef0910d784612a31dbd4bbfe02c5749d5b4
etes <ermias@npolar.no>
2014-04-04 10:53:54 +0200

    cleanup

--- js/iframe-main.js
+++ js/iframe-main.js
@@ -371,54 +371,6 @@ function initToolbar(map) {
 	});
 
 }
-// PLACENAMES SEARCH
-// Create projection definition of UTM33
-//Proj4js.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
-Proj4js.defs["EPSG:32633"] = "+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
-
-var wgs84 = new Proj4js.Proj('EPSG:4326');
-var utm33 = new Proj4js.Proj('EPSG:32633');
-var position, graphic;
-// transform placename coordinates from wgs84 to utm 33
-function WGS84ToUTM33(event) {
-	var north, east;
-	north = event.north;
-
-	east = event.east;
-	position = new Proj4js.Point(east, north);
-	// transform to UTM33
-	Proj4js.transform(wgs84, utm33, position);
-}
-
-var placenameGraphic;
-// Add point marker to map and zoom to that position
-function addMarker(position){
-	
-	map.graphics.remove(placenameGraphic);
-	
-	// uses Esri picture marker symbol
-	var symbol = new esri.symbol.PictureMarkerSymbol(
-			{
-				"angle" : 0,
-				"xoffset" : 0,
-				"yoffset" : 10,
-				"type" : "esriPMS",
-				// "url" : "/img/BluePin1LargeB.png",
-				"url" : "http://static.arcgis.com/images/Symbols/Shapes/BluePin1LargeB.png",
-				"contentType" : "image/png",
-				"width" : 24,
-				"height" : 24
-			});
-	placenameGraphic = new esri.Graphic(new esri.geometry.Point(position.x, position.y),
-			symbol);
-	map.graphics.add(placenameGraphic);
-	
-	// zoom to point coordinates
-	map.setExtent(new esri.geometry.Extent(position.x - 20000, position.y,
-			position.x + 20000, position.y, map.spatialReference));	
-}
-
-// END PLACENAMES SEARCH
 
 function addGraphic(geometry) {
 	iframetitleinput = $("#iframetitleinput").val();
@@ -664,6 +616,52 @@ function toggle_draw_toolbar() {
 dojo.ready(init);
 
 
+// PLACENAMES SEARCH
+// Create projection definition of UTM33
+//Proj4js.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+Proj4js.defs["EPSG:32633"] = "+proj=utm +zone=33 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+
+var wgs84 = new Proj4js.Proj('EPSG:4326');
+var utm33 = new Proj4js.Proj('EPSG:32633');
+var position, graphic;
+// transform placename coordinates from wgs84 to utm 33
+function WGS84ToUTM33(event) {
+	var north, east;
+	north = event.north;
+
+	east = event.east;
+	position = new Proj4js.Point(east, north);
+	// transform to UTM33
+	Proj4js.transform(wgs84, utm33, position);
+}
+
+var placenameGraphic;
+// Add point marker to map and zoom to that position
+function addMarker(position){
+	
+	map.graphics.remove(placenameGraphic);
+	
+	// uses Esri picture marker symbol
+	var symbol = new esri.symbol.PictureMarkerSymbol(
+			{
+				"angle" : 0,
+				"xoffset" : 0,
+				"yoffset" : 10,
+				"type" : "esriPMS",
+				// "url" : "/img/BluePin1LargeB.png",
+				"url" : "http://static.arcgis.com/images/Symbols/Shapes/BluePin1LargeB.png",
+				"contentType" : "image/png",
+				"width" : 24,
+				"height" : 24
+			});
+	placenameGraphic = new esri.Graphic(new esri.geometry.Point(position.x, position.y),
+			symbol);
+	map.graphics.add(placenameGraphic);
+	
+	// zoom to point coordinates
+	map.setExtent(new esri.geometry.Extent(position.x - 20000, position.y,
+			position.x + 20000, position.y, map.spatialReference));	
+}
 
 //placenames search using jquery autocomplete
    $(function() {
@@ -710,12 +708,14 @@ dojo.ready(init);
           };
   });
 
+// END PLACENAMES SEARCH
 
 //COLOR PICKER: uses specturm.js
 $("#colorpicker").spectrum({
     color: "#ff0000",
     //showButtons: false,
 });
+// disable enter key in input boxes
 $('#iframetitleinput').keypress(function(e){
     if ( e.which == 13 ) return false;
     //or...
@@ -726,3 +726,5 @@ $('#search-input').keypress(function(e){
     //or...
     if ( e.which == 13 ) e.preventDefault();
 });
+
+//ingen GIS data ble skadet i utviklingen av disse kartene.
