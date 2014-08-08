dbc766c8d2b726a6bbf7c5be1f954a1124e5772e
etes <ermias@npolar.no>
2014-08-08 15:39:10 +0200

    fix urls of skutersoner lovdata references

---
+++ bin/skutersoner.py
@@ -0,0 +1,32 @@
+'''
+Created on 8. aug. 2014
+
+@author: ermias
+'''
+import os
+import arcpy
+from arcpy import env
+
+env.workspace = "SMS_data_Svalbardkartet.gdb/Skuterbegrensning"
+
+inFeatureClass = "E:/Data/SMS/etrs89/SMS_data_Svalbardkartet.gdb/Skuterbegrensning/Skuterferdsel_Svalbkartet_0814"
+
+code_block_add_url = '''def add_url(lenke):
+    if len(lenke.split(',')) == 2:
+        return lenke.split(',')[1].strip()
+    else:
+        return ""
+'''
+
+code_block_fix_url = '''def fix_url(lenke):
+    if len(lenke.split(',')) == 2:
+        return lenke.split(',')[0].strip()
+    else:
+        return lenke
+'''
+
+#arcpy.AddField_management("Skuterferdsel_Svalbkartet_0814", 'Lenke2', "text", "", "", "255")
+print "Added new field Lenke2"
+arcpy.CalculateField_management(inFeatureClass,"Lenke2", "add_url(!Lenke!)","PYTHON",code_block_add_url)
+print "Added second url to lenke2"
+arcpy.CalculateField_management(inFeatureClass,"Lenke", "fix_url(!Lenke!)","PYTHON",code_block_fix_url)
