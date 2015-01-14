3efb58df690cca19fbff25ee04e5b49a1c01b51b
Ermias <ermias@npolar.no>
2015-01-14 14:52:48 +0100

    Fetch Proj4 definition from http://epsg.io by EPSG code

--- src/files/public/js/npmaps.js
+++ src/files/public/js/npmaps.js
@@ -1,82 +1,46 @@
 /*
  * A JavaScript library for interactive maps of Norwegian Polar Institute. Based on Proj4js library and Esri Javascript API
 
- * @requires proj4js-compressed.js
+ * @requires proj4.js
  * @requires ArcGIS API for Javascript
  */
 
-// Create projection definition of UTM33
-//Proj4js.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
-/*
-Proj4js.defs["EPSG:25833"] = "+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs";
-
-var wgs84 = new Proj4js.Proj('EPSG:4326');
-var utm33 = new Proj4js.Proj('EPSG:25833');
-var position, graphic;
-// transform placename coordinates from wgs84 to utm 33
-function WGS84ToUTM33(event) {
-	var north, east;
-	north = event.result.raw.north;
-
-	east = event.result.raw.east;
-	position = new Proj4js.Point(east, north);
-	// transform to UTM33
-	Proj4js.transform(wgs84, utm33, position);
-	//console.log(position);
-}
-
-// Add point marker to map and zoom to that position
-function addMarker(position){
-
-	// uses Esri picture marker symbol
-	var symbol = new esri.symbol.PictureMarkerSymbol(
-			{
-				"angle" : 0,
-				"xoffset" : 0,
-				"yoffset" : 10,
-				"type" : "esriPMS",
-				// "url" : "/img/BluePin1LargeB.png",
-				"url" : "http://static.arcgis.com/images/Symbols/Shapes/BluePin1LargeB.png",
-				"contentType" : "image/png",
-				"width" : 24,
-				"height" : 24
-			});
-	graphic = new esri.Graphic(new esri.geometry.Point(position.x, position.y),
-			symbol);
-	graphicLayer.clear();
-	graphicLayer.add(graphic);
-
-	// zoom to point coordinates
-	map.setExtent(new esri.geometry.Extent(position.x - 20000, position.y,
-			position.x + 20000, position.y, map.spatialReference));
-}
-*/
 var Npmaps = function(options) {
 
-	Proj4js.defs["EPSG:25833"] = "+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs";
-
-	var wgs84 = new Proj4js.Proj('EPSG:4326');
-	var utm33 = new Proj4js.Proj('EPSG:25833');
 	this.position,
 	this.graphic;
-
 	options = options || {};
 	this.map = options.map;
-	this.projection = options.project || "utm33";
+	this.projection = options.projection || '25833';
 
-	// transform placename coordinates from wgs84 to utm 33
+	// Fetches Proj4 definition from http://epsg.io
+	this._fetch = function(code) {
+		var script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.src = '//epsg.io/' + code + '.js';
+		document.getElementsByTagName('head')[0].appendChild(script);
+	}
+
+	// transform placename coordinates from wgs84 to utm
 	this.wgs84ToUtm = function(event) {
+		// Create projection definition if not known projection
+		try {
+			proj4('EPSG:' + this.projection);
+		} catch (e) {
+			this._fetch(this.projection);
+		}
+
+		var wgs84 = new proj4('EPSG:4326');
+		var utm = new proj4('EPSG:' + this.projection);
 		var north, east;
 		north = event.result.raw.north;
-
 		east = event.result.raw.east;
-		position = new Proj4js.Point(east, north);
-		// transform to UTM33
-		Proj4js.transform(wgs84, utm33, position);
-		//console.log(position);
+		// transform to UTM
+		this.position = proj4(wgs84,utm, [east, north]);
 	}
+
 	// Add point marker to map and zoom to that position
-	this.addMarker = function(position){
+	this.addMarker = function(xy){
 
 		// uses Esri picture marker symbol
 		var symbol = new esri.symbol.PictureMarkerSymbol(
@@ -91,14 +55,14 @@ var Npmaps = function(options) {
 				"width" : 24,
 				"height" : 24
 			});
-			graphic = new esri.Graphic(new esri.geometry.Point(position.x, position.y),
+			graphic = new esri.Graphic(new esri.geometry.Point(xy[0], xy[1]),
 			symbol);
 			graphicLayer.clear();
 			graphicLayer.add(graphic);
 
 			// zoom to point coordinates
-			this.map.setExtent(new esri.geometry.Extent(position.x - 20000, position.y,
-				position.x + 20000, position.y, map.spatialReference));
+			this.map.setExtent(new esri.geometry.Extent(xy[0] - 20000, xy[1],
+				xy[0] + 20000, xy[1], map.spatialReference));
 		}
 
 };
