b191772ccb35b13e3edaf03b88f9d65a1024b271
Ermias <ermias@npolar.no>
2015-01-16 16:57:08 +0100

    Draw any type of graphics with ArcGISJS Draw tool

--- src/files/public/js/npmaps.js
+++ src/files/public/js/npmaps.js
@@ -21,27 +21,26 @@ var Npmaps = function(options) {
 		document.getElementsByTagName('head')[0].appendChild(script);
 	}
 
-	// Create projection definition if not known projection
+	// Create projection definition if not a known projection
 	try {
 		proj4('EPSG:' + this.projection);
 	} catch (e) {
 		this._fetch(this.projection);
 	}
-	
+
 	// transform placename coordinates from wgs84 to utm
 	this.wgs84ToUtm = function(event) {
-
 		var wgs84 = new proj4('EPSG:4326');
 		var utm = new proj4('EPSG:' + this.projection);
 		var north, east;
 		north = event.result.raw.north;
 		east = event.result.raw.east;
 		// transform to UTM
-		this.position = proj4(wgs84,utm, [east, north]);
+		return proj4(wgs84,utm, [east, north]);
 	}
 
 	// Add point marker to map and zoom to that position
-	this.addMarker = function(xy){
+	this.addMarker = function(map, xy){
 
 		// uses Esri picture marker symbol
 		var symbol = new esri.symbol.PictureMarkerSymbol(
@@ -50,7 +49,6 @@ var Npmaps = function(options) {
 				"xoffset" : 0,
 				"yoffset" : 10,
 				"type" : "esriPMS",
-				// "url" : "/img/BluePin1LargeB.png",
 				"url" : "http://static.arcgis.com/images/Symbols/Shapes/BluePin1LargeB.png",
 				"contentType" : "image/png",
 				"width" : 24,
@@ -62,8 +60,39 @@ var Npmaps = function(options) {
 			graphicLayer.add(graphic);
 
 			// zoom to point coordinates
-			this.map.setExtent(new esri.geometry.Extent(xy[0] - 20000, xy[1],
+			map.setExtent(new esri.geometry.Extent(xy[0] - 20000, xy[1],
 				xy[0] + 20000, xy[1], map.spatialReference));
+		}
+
+		this.addGraphics = function (map, geometry) {
+
+			//create a random color for the symbols
+			var r = Math.floor(Math.random() * 250);
+			var g = Math.floor(Math.random() * 100);
+			var b = Math.floor(Math.random() * 100);
+
+			//Marker symbol used for point created using svg path. See this site for more examples
+			// http://raphaeljs.com/icons/#talkq. You could also create marker symbols using the SimpleMarkerSymbol class
+			//to define color, size, style or the PictureMarkerSymbol class to specify an image to use for the symbol.
+			var markerSymbol = new esri.symbol.SimpleMarkerSymbol(esri.symbol.SimpleMarkerSymbol.STYLE_CIRCLE, 20, new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color([r, g, b, 0.5]), 10), new dojo.Color([r, g, b, 0.9]));
+
+			//line symbol used for polyline.
+			var lineSymbol = new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_SOLID, new dojo.Color([r, g, b, 0.85]), 6);
+
+			//a simple fill symbol used for extent, polygon and freehand polygon.
+			var fillSymbol = new esri.symbol.SimpleFillSymbol(esri.symbol.SimpleFillSymbol.STYLE_SOLID, new esri.symbol.SimpleLineSymbol(esri.symbol.SimpleLineSymbol.STYLE_DASHDOT, new dojo.Color([r, g, b]), 2), new dojo.Color([r, g, b, 0.3]));
+
+			var type = geometry.type, symbol;
+			if (type === "point") {
+				symbol = markerSymbol;
+			} else if (type === "polyline") {
+				symbol = lineSymbol;
+			} else {
+				symbol = fillSymbol;
+			}
+
+			//Add the graphic to the map
+			map.graphics.add(new esri.Graphic(geometry, symbol));
 		}
 
 };
