09dc487a1cd66df50d09890f97a8fbec725eccc1
Ermias <ermias@npolar.no>
2015-02-04 12:01:33 +0100

    fix layer data source for marine mammals sightings

--- bin/embed.py
+++ bin/embed.py
@@ -5,6 +5,12 @@ mxd = arcpy.mapping.MapDocument(r"W:/NP/SvalbardTema/embed.mxd")
 layers = arcpy.mapping.ListLayers(mxd)[33]
 for layer in layers:
     if layer.symbologyType == "UNIQUE_VALUES":
-        print layer.symbology.classValues[0].decode('utf8')
-        layer.definitionQuery = "'" + layer.symbology.valueField + "' = '" + layer.symbology.classValues[0] + "'"
-
+        for layer in layers:
+            arcpy.env.workspace = layer.workspacePath
+            fcs = arcpy.ListFeatureClasses('','','sjopattedyrobservasjon')
+            for fc in fcs:
+                if layer.name.split(' ')[0] in fc:
+                    layer.replaceDataSource(layer.workspacePath, "FILEGDB_WORKSPACE", fc)
+                    layer.definitionQuery = ""
+                    print layer.name.encode('utf8')
+                    print layer.dataSource.encode('utf8')
