9f013df3bb79f0b57108a2955992865af6671cb0
Ermias <ermias@npolar.no>
2015-02-11 16:10:00 +0100

    use boundlessgeo gsconfig lib

--- bin/publish_wms.py
+++ bin/publish_wms.py
@@ -1,21 +1,37 @@
-#!/bin/sh
-geoserver=http://liv.npolar.no:8080/geoserver
-username=username
-password=password
-workspace=barentsportal
-format=GeoTIFF
-workspace_schema="<workspace><name>$workspace</name></workspace>"
-#create workspace
-#curl -v -u $username:$password -XPOST -H "Content-type: application/xml" -d $workspace_schema $geoserver"/rest/workspaces"
+import os
+from geoserver.catalog import Catalog
 
-#curl -v -u $username:$password -XPOST -H "Content-type: application/xml" -d "<coverageStore><name>raster</name><type>GeoTIFF</type><workspace>barentsportal</workspace><enabled>true</enabled></coverageStore>" "http://liv.npolar.no:8080/geoserver/rest/workspaces/barentsportal/coveragestores"
-data=data/barentsportal/polarbear_optimal_habitat/Change/
-curl -v -u $username:$password -XPUT -H "Content-type: image/tiff" --data-binary @dl/polarbear_optimal_habitat/Change/PolarBearOptimalHabitat_Change_April_1992-1996_to_2013.tif http://liv.npolar.no:8080/geoserver/rest/workspaces/barentsportal/coveragestores/PolarBearOptimalHabitat_Change_April_1992-1996_to_2013.tif/file.geotiff
+def publish_raster(geoserver_rest, username,password, in_raster, workspace):
+    file_name = os.path.splitext(os.path.basename(in_raster))[0]
+    dest= '/'.join([geoserver_rest, 'workspaces', workspace, 'coveragestores', file_name, 'file.geotiff'])
+    command = 'curl -v -u '+ username + ':' +password + ' -XPUT -H "Content-type: image/tiff" --data-binary @' + in_raster + ' ' + dest
+    os.system(command)
 
+def publish_sld(geoserver_rest, username, password, style_file, style_name):
+    style_def= "<style><name>" + style_name +"</name><filename>" + os.path.basename(style_file) + "</filename></style>"
+    curlString_style_def = 'curl -v -u ' + username + ':' + password + ' -XPOST -H "Content-type: text/xml" -d "' + style_def + '" ' + '/'.join([geoserver_rest, 'styles'])
+    dest= '/'.join([geoserver_rest, 'styles', style_name])
+    curlString_style = 'curl -v -u ' + username + ':' + password + ' -XPUT -H "Content-type: application/vnd.ogc.sld+xml" -d @' + style_file + ' ' + dest
+    os.system(curlString_style_def)
+    os.system(curlString_style)
 
-class publisher:
-    @staticmethod
-    def geoserver_url(env):
-        return 'http://liv.npolar.no.8080/geoserver'
+curl -u username:password -XPUT -H 'Content-type: text/xml' -d '<layer><defaultStyle><name>pboh_monthly_april</name></defaultStyle></layer>'\
+ http://liv.npolar.no:8080/geoserver/rest/layers/barentsportal:PolarBearOptimalHabitat_Montly_April_1992-1996
 
-    self.workspace_url =
+username = "admin"
+password = "password"
+geoserver_rest = 'http://liv.npolar.no:8080/geoserver/rest'
+cat = Catalog(geoserver_rest, username, password)
+
+in_rast = "E:/Data/Barentsportal/Biodiversity/Polarbear/PolarbearOptimalHabitat/Monthly/PolarBearOptimalHabitat_Montly_April_1992-1996.tif"
+file_name = os.path.splitext(os.path.basename(in_raster))[0]
+workspace= "barentsportal"
+cat.create_coveragestore(file_name, in_raster, workspace)
+
+cat = Catalog("http://liv.npolar.no:8080/geoserver/rest", username, password)
+while cat:
+    layer = cat.get_layer(file_name)
+layer_style = cat.get_style('pboh_monthly_april')
+layer.default_style = layer_style
+cat.save(layer)
+cat.reload()
